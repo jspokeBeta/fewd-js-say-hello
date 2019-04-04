22d29c61a0cf9c3dc067f6624f0626f0c8656a5a
jspokeBeta <47574993+jspokeBeta@users.noreply.github.com>
2019-04-04 19:03:19 -0400

    Added event handler

---
+++ main.js
@@ -0,0 +1,11 @@
+//Get input
+var input = document.querySelector('input');
+
+//Listen for enter key on input field
+input.addEventListener('keydown', function(event){
+  var userInput = input.value;
+  var Greeting = document.querySelector('.col-md-6');
+  var p = document.createElement('p');
+  
+}
+)
