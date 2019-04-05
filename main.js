a603af641faf6900458394b02767d2ba71e4f3d4
jspokeBeta <47574993+jspokeBeta@users.noreply.github.com>
2019-04-04 20:13:06 -0400

    Before the evil changes

--- main.js
+++ main.js
@@ -4,8 +4,21 @@ var input = document.querySelector('input');
 //Listen for enter key on input field
 input.addEventListener('keydown', function(event){
   var userInput = input.value;
-  var Greeting = document.querySelector('.col-md-6');
+  var greeting = document.querySelector('.col-md-6');
   var p = document.createElement('p');
-  
-}
-)
+
+//If enter key is pressed
+  if(event.key === 'Enter') {
+    if (userInput.length > 0) {
+    p.innerText = `Hello ${userInput}, nice to meet you!`;
+    greeting.appendChild(p);
+  } else {
+    p.innerText = `No name? It is still nice to meet you!`;
+    greeting.appendChild(p);
+  }
+    event.preventDefault();
+
+  //Clear input field
+  input.value ='';
+  }
+}, false);
